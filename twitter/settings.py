b4c235b0229e6c60e7dfe2196480be57ceff1aa7
mukul-py-cmd <mukul.agarwal.mat14@itbhu.ac.in>
2020-09-11 21:19:21 +0530

    pass

--- twitter/settings.py
+++ twitter/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'twitter',
         'USER': 'postgres',
-        'PASSWORD': 'alladin123',
+        'PASSWORD': '#',
         'HOST': 'localhost',
         'PORT': '5432',
     }
