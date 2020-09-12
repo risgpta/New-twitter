c5fea931bfcce6e1a48d0e99f15fc35d3f6ef1a0
risgpta <risgpta@gmail.com>
2020-09-13 00:42:14 +0530

    remove develop part from frontend

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import RegisterUser
-from rest_framework.authtoken import views as auth_views
-
-
-urlpatterns = [
-	path('register/', RegisterUser.as_view(),name = 'register-user'),
-    path('login/', auth_views.obtain_auth_token,name = 'login-user'),
-]
