1d0af3e02374cd49825c13da7ca916bed795baed
risgpta <risgpta@gmail.com>
2020-09-19 01:31:48 +0530

    deploy setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
+import { HashRouter as Router,Switch, Route } from "react-router-dom";
 import { CookiesProvider } from 'react-cookie';
 import Home from './home';
 import Newtwitter from './newtwitter';
