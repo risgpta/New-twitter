73688dd8144dfdb8e895fca148c35138f8c45370
Rishabh Gupta <risgpta@gmail.com>
2020-10-25 20:00:24 +0530

    Updates

---
+++ precache-manifest.e243ad1d81bcc175f846c736f24bd8a5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3400be53f32e78dcefe6fedd1c3c4867",
+    "url": "/twitter/index.html"
+  },
+  {
+    "revision": "7023c224c3ffd878c577",
+    "url": "/twitter/static/css/main.68ee3585.chunk.css"
+  },
+  {
+    "revision": "8cdfac7bf02b1704a613",
+    "url": "/twitter/static/js/2.a42947f4.chunk.js"
+  },
+  {
+    "revision": "8e8a87852e476777f7b36c421326ae8d",
+    "url": "/twitter/static/js/2.a42947f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7023c224c3ffd878c577",
+    "url": "/twitter/static/js/main.be6efdef.chunk.js"
+  },
+  {
+    "revision": "1af2ebb3fe3230859338",
+    "url": "/twitter/static/js/runtime-main.66254f84.js"
+  },
+  {
+    "revision": "085a38ebb626afe15a473b7b986bf7a6",
+    "url": "/twitter/static/media/add.085a38eb.svg"
+  },
+  {
+    "revision": "f2103d04e38fd4f5b096c7b008e62896",
+    "url": "/twitter/static/media/email.f2103d04.svg"
+  },
+  {
+    "revision": "822330c7c272730473b5d4d1794031cd",
+    "url": "/twitter/static/media/heart.822330c7.svg"
+  },
+  {
+    "revision": "30351fcf3e69dde33bd4b60600f51292",
+    "url": "/twitter/static/media/person.30351fcf.svg"
+  },
+  {
+    "revision": "91ee1ebee1d08569f5b84fbf4cd8a9db",
+    "url": "/twitter/static/media/twitter.91ee1ebe.svg"
+  },
+  {
+    "revision": "ea28a43cd38df5a645a6b0074c5ea685",
+    "url": "/twitter/static/media/unlikeheart.ea28a43c.svg"
+  }
+]);
